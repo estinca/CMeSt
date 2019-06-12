fea9ca79d374e2a80e43b26071e13986b7355110
Erast Stinca <eric.stinca@gmail.com>
2019-06-12 10:24:40 +0300

    initial commit; modules: cms frontend (angular) +  backend, repository api + dbm and utils

---
+++ cms/server/src/test/java/com/est/cms/CMSApplication.java
@@ -0,0 +1,13 @@
+package com.est.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMSApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CMSApplication.class, args);
+	}
+
+}
